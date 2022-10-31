a4c9a4a7c425ed5d91a7483faad14924d964d5a3
Hamza Erraji <heerraji123@gmail.com>
2022-10-31 19:48:58 +0100

    fix bug

--- app/models.py
+++ app/models.py
@@ -1,13 +1,1 @@
-from .database import base
-from sqlalchemy import Integer, column, String, Boolean
-from sqlalchemy.sql.expression import null
 
-
-
-
-class Post(base):
-    __tablename__= "posts"
-    id = column(Integer, primary_key=True, nullable=False)
-    title= column(String , nullable= False)
-    content= column(String , nullable=False)
-    published = column(Boolean , default = True )
